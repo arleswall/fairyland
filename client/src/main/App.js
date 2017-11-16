c49cd2c0707308f245aecfc42d56af59a4921b2d
Arles Wallauer <arleswallauer@gmail.com>
2017-11-16 12:18:00 -0700

    Code fixes

--- client/src/main/App.js
+++ client/src/main/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./views/Home";
 import CupcakesContainer from "./views/cupcakes/Container";
 import OrderContainer from "./views/order/Container";
--- client/src/main/views/admin/ListComponent.js
+++ client/src/main/views/admin/ListComponent.js
@@ -5,63 +5,63 @@ import {connect} from "react-redux";
 import * as actions from "../../../redux/auth";
 
 class ListComponent extends React.Component{
-  constructor(props) {
-    super();
-    this.state = {
-      order: []
-    };
-   this.handleEdit = this.handleEdit.bind(this);
-  }
-  componentDidMount() {
-    this.props.verify();  
-    axios.get(`http://localhost:8000/admin/orders`).then(response => {
-      this.setState({order: response.data});
-    })
-  }
-  
-  handleEdit(event, id){
-  event.persist();
-  axios.put("http://localhost:8000/admin/orders/" + id, {completed: event.target.checked})
-    .then((response)=>{
-      this.setState((prevState)=>{
-        let updatedOrder = [...prevState.order];
-         updatedOrder = updatedOrder.map(order => {
-          if (order._id === id){
-          return response.data
-        } else {
-          return order
-        }
-      })
-        return {
-          order: updatedOrder
-        }
-    })
-
-  })
-}
-  
-  render(){
-  return(
-        <div className="omsBackground">
-            {this.state.order.sort((a, b)=>{
-              let firstTime = new Date(a.customer.pickUpTime).getTime();
-              let secondTime = new Date(b.customer.pickUpTime).getTime();
-              return firstTime - secondTime;
-            }).map((order, i)=>{
-              return(    
-                      <AdminComponent order={order}
-                                      key={order._id + i}
-                                      id={order._id}
-                                      handleEdit={this.handleEdit}
-                                      />            
-                
-              )
-            })
-          }
-        </div>
+    constructor(props) {
+        super();
+        this.state = {
+            order: []
+        };
+        this.handleEdit = this.handleEdit.bind(this);
+    }
     
-  )
-  }
+    componentDidMount() {
+        this.props.verify();  
+        axios.get(`http://localhost:8000/admin/orders`).then(response => {
+            this.setState({order: response.data});
+        })
+    }
+  
+    handleEdit(event, id) {
+        event.persist();
+        axios.put("http://localhost:8000/admin/orders/" + id, {
+            completed: event.target.checked
+        })
+        .then((response) => {
+            this.setState((prevState) => {
+                let updatedOrder = [...prevState.order];
+                updatedOrder = updatedOrder.map(order => {
+                    if (order._id === id) {
+                        return response.data
+                    } else {
+                        return order
+                    }
+                })
+                return {
+                    order: updatedOrder
+                }
+            })
+        })
+    }
+  
+    render(){
+        return(
+            <div className="omsBackground">
+                {this.state.order.sort((a, b)=>{
+                    let firstTime = new Date(a.customer.pickUpTime).getTime();
+                    let secondTime = new Date(b.customer.pickUpTime).getTime();
+                    return firstTime - secondTime;
+                }).map((order, i)=>{
+                    return(    
+                        <AdminComponent 
+                            order={order} 
+                            key={order._id + i}
+                            id={order._id}
+                            handleEdit={this.handleEdit}/>            
+                    )
+                })
+              }
+            </div>
+        )
+    }
 }
 
 export default connect(null, actions)(ListComponent);
